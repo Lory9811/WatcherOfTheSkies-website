8c612def5b7102384bdaf426b904285aa268fa04
Lorenzo <lorenzo.98m@gmail.com>
2017-05-15 21:43:00 +0200

    Added login

--- static/scripts/manager.js
+++ static/scripts/manager.js
@@ -1,8 +1,8 @@
 const pages = ['home', 'videos', 'events', 'info', 'diag'];
 var currentPageId = 0;
 var prevPage = 0;
-var diag_canvas = null;
-var diag_canvas_context = null;
+var diagCanvas = null;
+var diagCanvasContext = null;
 
 window.onresize = function() {
 	update();
@@ -13,15 +13,15 @@ function update() {
 }
 
 function changePage() {
-	if ($(this).parent().hasClass("active") && !$(this).hasClass("btn")) {
+	if ($(this).parent().hasClass("active") && !$(this).hasClass("btn") && !$(this).hasClass("video-bar-link")) {
 		return;
 	} else {
-		console.log($(this).data("page"));
 		transition(pages[currentPageId], $(this).data("page"), pages[currentPageId] != "diag" ? $(this).data("orientation") : "horizontal");
 	}
 }
 
 function transition(currentPage, nextPage, orientation) {
+	console.log(currentPageId);
 	$('#' + currentPage).addClass(orientation == "vertical" ? "hv" : "hh");
 	$('#' + currentPage).removeClass("active");
 	$('.' + currentPage + '-btn').removeClass("active");
@@ -36,33 +36,17 @@ function transition(currentPage, nextPage, orientation) {
 		$('#' + currentPage).addClass("hidden");
 	}, 500);
 	currentPageId = pages.indexOf(nextPage);
-	console.log(currentPageId);
 	update();
 }
 
-//disabled for now
-/*window.onwheel = function(event) {
-	currentPage = pages.indexOf(window.location.hash.substr(1));
-	if (currentPage == -1) currentPage = 0;
-	
-	prevPage = currentPage;
-	if (currentPage == 0) {
-		if (event.deltaY > 0) {
-			currentPage += 1;
-		}
-	} else if (currentPage == pages.length - 1) {
-		if (event.deltaY < 0) {
-			currentPage -= 1;
-		}
+function toggleLogin() {
+	login = $('#login-popup');
+	if (login.hasClass("hidden")) {
+		login.removeClass("hidden");
 	} else {
-		currentPage += (event.deltaY / Math.abs(event.deltaY));
+		login.addClass("hidden");
 	}
-	window.location.hash = pages[currentPage];
-	$('#' + pages[prevPage] + "-btn").removeClass("active");
-	$('#' + pages[currentPage] + "-btn").addClass("active");
-	
-	return false;
-};*/
+}
 
 $(document).ready(
 	function() {
@@ -70,32 +54,27 @@ $(document).ready(
 			$('.' + page + '-btn-lnk').click(changePage);
 		});
 		
+		$('#login').click(toggleLogin);
+		
 		update();
 		
 		// fake diagnostics graph in homepage
-		diag_canvas = document.getElementById('diag-canvas');
-		diag_canvas_context = diag_canvas.getContext('2d');
+		diagCanvas = document.getElementById('diag-canvas');
+		diagCanvasContext = diagCanvas.getContext('2d');
 		
 		setInterval(
 		function() {
-			diag_canvas_context.clearRect(0, 0, diag_canvas.width, diag_canvas.height);
-			diag_canvas_context.fillStyle='orangered';
-			diag_canvas_context.fillRect(0, 20, randomRange(0, 100), 10);
-			//diag_canvas_context.stroke();
-			diag_canvas_context.fillRect(0, 35, randomRange(0, 100), 10);
-			//diag_canvas_context.stroke();
-			diag_canvas_context.fillRect(0, 50, randomRange(0, 100), 10);
-			//diag_canvas_context.stroke();
-			diag_canvas_context.fillRect(0, 65, randomRange(0, 100), 10);
-			//diag_canvas_context.stroke();			
-			diag_canvas_context.fillRect(0, 80, randomRange(0, 100), 10);
-			//diag_canvas_context.stroke();
-			diag_canvas_context.fillRect(0, 95, randomRange(0, 100), 10);
-			//diag_canvas_context.stroke();
-			diag_canvas_context.fillRect(0, 110, randomRange(0, 100), 10);
-			//diag_canvas_context.stroke();
-			diag_canvas_context.fillRect(0, 125, randomRange(0, 100), 10);
-			diag_canvas_context.stroke();
+			diagCanvasContext.clearRect(0, 0, diagCanvas.width, diagCanvas.height);
+			diagCanvasContext.fillStyle='orangered';
+			diagCanvasContext.fillRect(0, 20, randomRange(0, 100), 10);
+			diagCanvasContext.fillRect(0, 35, randomRange(0, 100), 10);
+			diagCanvasContext.fillRect(0, 50, randomRange(0, 100), 10);
+			diagCanvasContext.fillRect(0, 65, randomRange(0, 100), 10);		
+			diagCanvasContext.fillRect(0, 80, randomRange(0, 100), 10);
+			diagCanvasContext.fillRect(0, 95, randomRange(0, 100), 10);
+			diagCanvasContext.fillRect(0, 110, randomRange(0, 100), 10);
+			diagCanvasContext.fillRect(0, 125, randomRange(0, 100), 10);
+			diagCanvasContext.stroke();
 		}, 200);
 });
 
